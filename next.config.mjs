5b5641634cfd71fb3747a60ca1f803eb3610b7a9
Marc Pomar <marc@faable.com>
2026-03-31 12:08:12 +0200

    fix: base path

--- next.config.mjs
+++ next.config.mjs
@@ -8,4 +8,5 @@ const withNextra = nextra({
 // Export the final Next.js config with Nextra included
 export default withNextra({
   // ... Add regular Next.js options here
+  basePath: "/docs",
 });
