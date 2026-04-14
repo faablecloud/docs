cf4662f73a191fd06d6a611fce5869f614a52faf
Marc Pomar <marc@faable.com>
2026-04-14 12:01:29 +0200

    fix: deploy better docs opengraph

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -3,10 +3,32 @@ import { useMDXComponents as getMDXComponents } from '../../mdx-components'
  
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
  
-export async function generateMetadata(props) {
+export async function generateMetadata(props, parent) {
   const params = await props.params
   const { metadata } = await importPage(params.mdxPath)
-  return metadata
+  const previousMetadata = await parent
+  
+  const path = params.mdxPath ? params.mdxPath.join('/') : ''
+  
+  return {
+    ...metadata,
+    alternates: {
+      canonical: `https://faable.com/docs/${path}`,
+    },
+    openGraph: {
+      ...previousMetadata.openGraph,
+      ...metadata.openGraph,
+      title: metadata.title,
+      description: metadata.description,
+      url: `https://faable.com/docs/${path}`,
+    },
+    twitter: {
+      ...previousMetadata.twitter,
+      ...metadata.twitter,
+      title: metadata.title,
+      description: metadata.description,
+    }
+  }
 }
  
 const Wrapper = getMDXComponents().wrapper
